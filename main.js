bf1de34b4de94aaeceed01d4c88971ed4231d8f7
maxiangan <maxiangan@aiforward.com>
2021-09-27 16:29:03 +0800

    首页内容完成

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import Vant from 'vant'
-
+// import 'vant/lib/index.less'
 
 Vue.config.productionTip = false
 
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -18,13 +18,28 @@
 				</view>
 			</view>
 		</view>
-		<ul class="serve" >
+		<ul class="serve">
 			<li v-for="item in serveList">
-				<view 	class="serve-item"></view>
+				<image v-if="item.src" src="item.src"></image>
+				<view v-else class="serve-item"></view>
 				<span>{{item.name}}</span>
 			</li>
 		</ul>
+		<view class="theme">
+			<image src="../../static/4.jpeg" class="theme-image"></image>
+		</view>
+		<ul class="card">
+			<li class="card-item" v-for="(item,index) in cardList" :key="index">
 
+				<image :src="item.src"></image>
+				<p>
+					{{item.marke}}
+				</p>
+				<p class="card-item-yuan">
+					<span>¥</span><span>{{item.yuan}}</span>
+				</p>
+			</li>
+		</ul>
 	</view>
 </template>
 <script>
@@ -38,63 +53,116 @@
 				duration: 500,
 				serveList: [{
 						name: "商城超市",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "商城家电",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "商城手机",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "商城服装",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "新人福利",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "促销打折",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "低价爆款",
-						src: "../../static/logo.png"
+
 					},
 					{
 						name: "商城超市",
-						src: "../../static/logo.png"
+
 					},
 
+				],
+
+				cardList: [{
+						marke: "日式餐具焗饭碗青花瓷拉泡面碗家用仿复古7.5寸韩式陶瓷双耳汤碗",
+						yuan: 34
+					},
+					{
+						marke: "日式餐具焗饭碗青花瓷拉泡面碗家用仿复古7.5寸韩式陶瓷双耳汤碗",
+						yuan: 26
+					}
 				]
 			}
 		},
-		methods: {
-
-		}
+		created() {
+			this.cardList.forEach((item, index) => {
+				item.src = (require('../../static/' + 10 + index + '.webp'))
+			})
+		},
 	}
 </script>
-<style scoped >
+<style scoped lang="less">
 	.serve {
 		margin: 10px;
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
-		justify-content:space-around
-	
+		justify-content: space-around;
+
+		.serve-item {
+			margin: 7px;
+			width: 50px;
+			height: 50px;
+			background-color: #4CD964;
+		}
+
+		span {
+			font-size: 14px;
+		}
 	}
-	.serve-item{
-	   margin: 7px;
-				width: 50px;
-				height: 50px;
-				background-color:#4CD964;
-					
-			
+
+
+
+	.theme-image {
+		width: 100%;
+		height: 100px;
+	}
+
+	.card {
+		margin-top: 20px;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-around;
+
+		.card-item {
+			border-radius: 5px;
+			width: 45%;
+			height: 185px;
+			border: 1px solid #C0C0C0;
+
+			p {
+				width: 100%;
+				font-size: 12px;
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+				-webkit-line-clamp: 2;
+				overflow: hidden;
 			}
-			span{
-				font-size: 14px;
+
+			image {
+				width: 100%;
+				height: 120px;
 			}
+
+			.card-item-yuan {
+				padding-top: 10px;
+				padding-left: 10px;
+				color: red;
+			}
+		}
+	}
 </style>
